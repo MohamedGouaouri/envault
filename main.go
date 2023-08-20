377c6449ed5e0a9e27e470df9263756612e490b0
Ubuntu <ubuntu@ip-172-31-9-122.eu-west-3.compute.internal>
2023-08-20 08:27:17 +0000

    Remove irrelevant comments

--- main.go
+++ main.go
@@ -6,28 +6,4 @@ import (
 
 func main() {
 	cmd.Execute()
-	// ctx := context.Background()
-
-	// // prepare a client with the given base address
-	// client, err := vault.New(
-	// 	vault.WithAddress("http://127.0.0.1:8200"),
-	// 	vault.WithRequestTimeout(30*time.Second),
-	// )
-
-	// if err != nil {
-	// 	log.Fatalf("Error connectin %v", err)
-	// }
-
-	// // authenticate with a root token (insecure)
-	// if err := client.SetToken("hvs.1ibNc9rOM6azW8ST2YyOaEHn"); err != nil {
-	// 	log.Fatalf("Error authenticating %v", err)
-	// }
-
-	// // read the secret
-	// s, err := client.Secrets.KvV2Read(ctx, "dev", vault.WithMountPath("secret"))
-	// if err != nil {
-	// 	log.Fatalf("Error reading %v", err)
-	// }
-	// log.Println("secret retrieved:", s.Data.Data["DB_URI"])
-
 }
